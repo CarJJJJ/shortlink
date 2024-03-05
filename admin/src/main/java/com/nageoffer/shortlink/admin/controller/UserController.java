e1c197304b5b2113063a531fa2138d7705c97587
CarJJJJ <893896371@qq.com>
2024-03-06 00:22:30 +0800

    add UserController

---
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -0,0 +1,21 @@
+package com.nageoffer.shortlink.admin.controller;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 用户管理层
+ */
+@RestController
+public class UserController {
+    /**
+     * 根据用户名查询用户信息
+     */
+    @GetMapping("/api/shortlink/v1/user/{username}")
+    public String getByUserName(@PathVariable("username") String username){
+        return "Hello" + username;
+    }
+
+}
