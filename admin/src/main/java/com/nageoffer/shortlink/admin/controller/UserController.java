35d2ceefcea12197a479bcdee2f61a038b85aaa6
CarJJJJ <893896371@qq.com>
2024-03-06 11:59:14 +0800

    revise README.md

--- admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/controller/UserController.java
@@ -1,6 +1,9 @@
 package com.nageoffer.shortlink.admin.controller;
 
 
+import com.nageoffer.shortlink.admin.dto.resp.UserRespDTO;
+import com.nageoffer.shortlink.admin.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,13 +12,16 @@ import org.springframework.web.bind.annotation.RestController;
  * 用户管理层
  */
 @RestController
+@RequiredArgsConstructor
 public class UserController {
+
+    private final UserService userService;
     /**
      * 根据用户名查询用户信息
      */
     @GetMapping("/api/shortlink/v1/user/{username}")
-    public String getByUserName(@PathVariable("username") String username){
-        return "Hello" + username;
+    public UserRespDTO getByUserName(@PathVariable("username") String username){
+        return userService.getUserByUsername(username);
     }
 
 }
