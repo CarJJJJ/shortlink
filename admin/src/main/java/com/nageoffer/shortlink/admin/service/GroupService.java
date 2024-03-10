55148ddd159ba709ab83db80e05aa3cbd222e45f
CarJJJJ <893896371@qq.com>
2024-03-10 14:50:06 +0800

    修改注册用户创建默认分组的bug

--- admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/GroupService.java
@@ -19,6 +19,8 @@ public interface GroupService extends IService<GroupDO> {
      */
     void saveGroup(String groupName);
 
+    void saveGroup(String username, String groupName);
+
 
 
     /**
--- admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
+++ admin/src/main/java/com/nageoffer/shortlink/admin/service/impl/GroupServiceImpl.java
@@ -1,11 +1,13 @@
 package com.nageoffer.shortlink.admin.service.impl;
 
 import cn.hutool.core.bean.BeanUtil;
+import cn.hutool.core.collection.CollUtil;
 import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.LambdaUpdateWrapper;
 import com.baomidou.mybatisplus.core.toolkit.Wrappers;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.nageoffer.shortlink.admin.common.biz.user.UserContext;
+import com.nageoffer.shortlink.admin.common.convention.exception.ClientException;
 import com.nageoffer.shortlink.admin.dao.entity.GroupDO;
 import com.nageoffer.shortlink.admin.dao.mapper.GroupMapper;
 import com.nageoffer.shortlink.admin.dto.req.ShortLinkGroupSortReqDTO;
@@ -14,10 +16,14 @@ import com.nageoffer.shortlink.admin.dto.resp.ShortLinkGroupRespDTO;
 import com.nageoffer.shortlink.admin.service.GroupService;
 import com.nageoffer.shortlink.admin.toolkit.RandomGenerator;
 import lombok.RequiredArgsConstructor;
+import org.redisson.api.RLock;
 import org.redisson.api.RedissonClient;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
+import static com.nageoffer.shortlink.admin.common.constant.RedisCacheConstant.LOCK_GROUP_CREATE_KEY;
 
 /**
  * 分组接口实现层
@@ -28,22 +34,47 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
 
     private final RedissonClient redissonClient;
 
+
+//    @Value("${short-link.group.max-num}")
+//    private Integer groupMaxNum;
     @Override
     public void saveGroup(String groupName) {
-        String gid;
-        do {
-            gid = RandomGenerator.generateRandom();
-        } while (!hasGid(gid));
-        GroupDO groupDO = GroupDO.builder()
-                .gid(gid)
-                .name(groupName)
-                .sortOrder(0)
-                .username(UserContext.getUsername())
-                .build();
-        baseMapper.insert(groupDO);
+        saveGroup(UserContext.getUsername(), groupName);
+    }
+
+    @Override
+    public void saveGroup(String username, String groupName) {
+        RLock lock = redissonClient.getLock(String.format(LOCK_GROUP_CREATE_KEY, username));
+        lock.lock();
+        try {
+            LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
+                    .eq(GroupDO::getUsername, username)
+                    .eq(GroupDO::getDelFlag, 0);
+            List<GroupDO> groupDOList = baseMapper.selectList(queryWrapper);
+//            if (CollUtil.isNotEmpty(groupDOList) && groupDOList.size() == groupMaxNum) {
+//                throw new ClientException(String.format("已超出最大分组数：%d", groupMaxNum));
+//            }
+            if (CollUtil.isNotEmpty(groupDOList) && groupDOList.size() == 10) {
+                throw new ClientException(String.format("已超出最大分组数：%d", 10));
+            }
+            String gid;
+            do {
+                gid = RandomGenerator.generateRandom();
+            } while (!hasGid(username, gid));
+            GroupDO groupDO = GroupDO.builder()
+                    .gid(gid)
+                    .sortOrder(0)
+                    .username(username)
+                    .name(groupName)
+                    .build();
+            baseMapper.insert(groupDO);
+        } finally {
+            lock.unlock();
+        }
     }
     @Override
     public List<ShortLinkGroupRespDTO> listGroup() {
+        String username = UserContext.getUsername();
         LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getDelFlag, 0)
                 .eq(GroupDO::getUsername, UserContext.getUsername())
@@ -87,11 +118,10 @@ public class GroupServiceImpl extends ServiceImpl<GroupMapper, GroupDO> implemen
             baseMapper.update(groupDO, updateWrapper);
         });
     }
-    private boolean hasGid(String gid) {
+    private boolean hasGid(String username, String gid) {
         LambdaQueryWrapper<GroupDO> queryWrapper = Wrappers.lambdaQuery(GroupDO.class)
                 .eq(GroupDO::getGid, gid)
-                // TODO 设置用户名
-                .eq(GroupDO::getUsername,UserContext.getUsername());
+                .eq(GroupDO::getUsername, Optional.ofNullable(username).orElse(UserContext.getUsername()));
         GroupDO hasGroupFlag = baseMapper.selectOne(queryWrapper);
         return hasGroupFlag == null;
     }
